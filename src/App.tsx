3e1b7f1a2ad4c5ab847d604ca0d99107bbe70df1
DESK-TDP-01\bobpham <bobpham.tdp@gmail.com>
2023-10-11 17:21:51 +1100

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Demo from "./components/Demo";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Demo />
     </>
   );
 }
